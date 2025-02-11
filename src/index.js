2a6eb35677b4d7ee302499bf82bde6c7517159e4
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2025-02-11 11:58:28 +0100

    config: Add inert

--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
 'use strict';
 
 const Hapi = require('@hapi/hapi');
+const Path = require('path');
 
 const init = async () => {
 
     const server = Hapi.server({
         port: 3000,
-        host: 'localhost'
+        host: 'localhost',
+        routes: {
+            files: {
+                relativeTo: Path.join(__dirname)
+            }
+        }
     });
+
+    await server.register(require('@hapi/inert'))
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+            return 'Hello World!';
+        }
+    })
+    server.route({
+        method: 'GET',
+        path: '/page1',
+        handler: (request, h) => {
+            return h.file('./page1.html');
+        }
+    })
 
     await server.start();
     console.log('Server running on %s', server.info.uri);
