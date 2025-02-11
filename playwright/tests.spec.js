5edfd687ee784ee8ed4a0ab270227b6bb674cda1
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2025-02-11 12:40:59 +0100

    tests: Add Playwright scenarii

---
+++ playwright/tests.spec.js
@@ -0,0 +1,25 @@
+import { test, expect } from '@playwright/test';
+
+test('As a bot', async ({ page }) => {
+    await page.goto('http://localhost:3000/page1');
+
+    const link = page.getByRole('link');
+    await link.click();
+
+    expect(page).toHaveURL(/\/page2$/)
+})
+
+test('As a human', async ({ page }) => {
+    await page.goto('http://localhost:3000/page1');
+
+    await sleep(1000);
+
+    const link = page.getByRole('link');
+    await link.click();
+
+    expect(page).toHaveURL(/\/page2$/)
+})
+
+async function sleep(duration) {
+    return new Promise(resolve => setTimeout(() => resolve(), duration))
+}
