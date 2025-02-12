869f50e25bddbaab981318da08d6d6b0a29c37f1
Gauthier Fiorentino <gauthier.fiorentino@octo.com>
2025-02-12 11:29:50 +0100

    tests: Update tests and add honeypot tests

--- playwright/tests.spec.js
+++ playwright/tests.spec.js
@@ -1,23 +1,47 @@
 import { test, expect } from '@playwright/test';
 
-test('As a bot', async ({ page }) => {
-    await page.goto('http://localhost:3000/page1');
+test.describe('timed-based captcha', () => {
+    test('As a bot', async ({ page }) => {
+        await page.goto('http://localhost:3000/page1');
 
-    const link = page.getByRole('link');
-    await link.click();
+        const link = page.getByRole('link');
+        await link.click();
 
-    expect(page).toHaveURL(/\/captcha$/)
+        expect(page).toHaveURL(/\/captcha/)
+    })
+
+    test('As a human', async ({ page }) => {
+        await page.goto('http://localhost:3000/page1');
+
+        await sleep(1000);
+
+        const link = page.getByRole('link');
+        await link.click();
+
+        expect(page).toHaveURL(/\/page2/)
+    })
 })
 
-test('As a human', async ({ page }) => {
-    await page.goto('http://localhost:3000/page1');
+test.describe('Honeypot', () => {
+    test('As a bot', async ({ page }) => {
+        await page.goto('http://localhost:3000/captcha?redirect=page1');
 
-    await sleep(1000);
+        const honeypot = page.getByRole('textbox').last();
+        await honeypot.fill("test");
+        const submit = page.getByRole('button')
+        await submit.click()
 
-    const link = page.getByRole('link');
-    await link.click();
+        expect(page).toHaveURL(/\/captcha/)
+    })
 
-    expect(page).toHaveURL(/\/page2$/)
+    test('As a human', async ({ page }) => {
+        await page.goto('http://localhost:3000/captcha?redirect=page1');
+
+        const submit = page.getByRole('button')
+        await submit.click()
+
+        expect(page).toHaveURL(/\/page1/)
+    })
 })
 
 async function sleep(duration) {
